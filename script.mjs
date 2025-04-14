2715d84fb1e78fbc518665edf51d2c31b64814f1
Geiselle Holt <geiselleholt@hotmail.com>
2025-04-14 01:00:20 -0600

    game working!

--- script.mjs
+++ script.mjs
@@ -33,11 +33,11 @@ let allCharactersImages = [
     image: "images/mance.png",
   },
   {
-    name: `Eddard "Ned" Stari`,
+    name: `Eddard "Ned" Stark`,
     image: "images/ned.png",
   },
   {
-    name: "Robb Stari",
+    name: "Robb Stark",
     image: "images/robb.png",
   },
   {
@@ -57,19 +57,23 @@ let allCharactersImages = [
     image: "images/walder.png",
   },
   {
-    name: "Bran Stari",
+    name: "Bran Stark",
     image: "images/bran.png",
   },
 ];
 
 let characterSelections = [];
+let characterSection = document.getElementById(`characterSection`);
+let selectedChar = "";
 
 (async function randomQuote() {
   const response = await axios("https://api.gameofthronesquotes.xyz/v1/random");
-  let quote = response.data.sentence;
   let person = response.data.character.name;
-  let quoteContainer = document.getElementById("quoteContainer");
-  quoteContainer.textContent = `"${quote}"`;
+  selectedChar += person;
+  console.log(selectedChar);
+  let quote = response.data.sentence;
+  let quoteSection = document.getElementById("quoteSection");
+  quoteSection.textContent = `"${quote}"`;
   let charImg = await getCharactersImages();
   charImg.forEach((char) => {
     if (char.name === person) {
@@ -79,19 +83,19 @@ let characterSelections = [];
   displayCharacters();
 })();
 
-function getCharSelections() {
-  const indexes = [];
-  while (indexes.length < 5) {
-    const randomIndexes = Math.floor(Math.random() * allCharactersImages.length);
-    if (!indexes.includes(randomIndexes)) {
-      indexes.push(randomIndexes);
-      characterSelections.push(allCharactersImages[randomIndexes]);
+function getCharacterSelections() {
+  const randomIndexes = [];
+  while (randomIndexes.length < 5) {
+    const randomIndex = Math.floor(Math.random() * allCharactersImages.length);
+    if (!randomIndexes.includes(randomIndex)) {
+      randomIndexes.push(randomIndex);
+      characterSelections.push(allCharactersImages[randomIndex]);
     }
   }
 }
 
 function displayCharacters() {
-  getCharSelections();
+  getCharacterSelections();
   const randomIndexes = [];
   while (randomIndexes.length < characterSelections.length) {
     const index = Math.floor(Math.random() * characterSelections.length);
@@ -99,11 +103,10 @@ function displayCharacters() {
       randomIndexes.push(index);
     }
   }
-  randomIndexes.forEach((index) => {   
-    let char = characterSelections[index]
+  randomIndexes.forEach((index) => {
+    let char = characterSelections[index];
     createCharacter(char.image, char.name);
   });
-  
 }
 
 function createCharacter(image, name) {
@@ -118,7 +121,6 @@ function createCharacter(image, name) {
 
   fragment.appendChild(clone);
 
-  let characterSection = document.getElementById(`characterList`);
   let charDiv = document.createElement(`li`);
   characterSection.appendChild(charDiv);
   charDiv.appendChild(fragment);
@@ -134,6 +136,33 @@ async function getCharactersImages() {
     allCharactersImages.push(imageNames);
   });
   return allCharactersImages;
+}
+
+characterSection.addEventListener(`click`, handleCharacterSection);
+
+function handleCharacterSection(e) {
+  e.preventDefault();
+  selectedImage = "";
+  if (e.target.tagName === "UL") {
+    console.log("click");
+    return;
+  } else {
+    console.log(selectedChar);
+
+    allCharactersImages.forEach((char) => {
+      if (char.name === selectedChar) {
+        selectedImage = char.image;
+        console.log(selectedImage);
+        console.log(e.target.src);
+      }
+    });
+    if (e.target.src === selectedImage || e.target.src === `http://127.0.0.1:5500/${selectedImage}`) {
+      alert("Correct 😀 \nPlay again!");
+      setTimeout(window.location.reload(), 2000);
+    } else {
+      alert("Nope 😵 Try Again");
+    }
+  }
 }
 
 // USED THIS FUNCTION TO GET LIST OF CHARACTERS IN THIS QUOTE API TO CROSS REFERENCE WITH THE CHARACTERS IN THE IMAGE API IN ORDER TO HARD CODE THE allCharactersImages ARRAY WITH CHARACTERS THAT WERE MISSING
